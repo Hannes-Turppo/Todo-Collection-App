8fcf86d4ffdff6a760a232b7d441c9f333653807
Hannes Turppo <hannes.turppo@gmail.com>
2025-02-26 00:17:39 +0200

    Worked on frontend today. After a few iterations, the AppBar is finally working properly and tomorrow I can start with register and login funvtionality.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,32 @@
 import React, { useState } from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-import MenuAppBar from "./components/menuAppBar"
-import { Box } from "@mui/material"
+import MenuAppBar from "./components/appbar/menuAppBar"
 import './App.css'
+
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <MenuAppBar />
-        <Routes>
+          <MenuAppBar />
+          <Routes>
 
-          {/* Homepage here shows user's collections. redirect to login if no token is found. */}
-          <Route path="/">
+            {/* Homepage here shows user's collections. redirect to login if no token is found. */}
+            <Route path="/">
+              
+            </Route>
+
+            {/* Register page here. Asks user for relevant info. If registration succeeds, redirect to login. */}
+            <Route path="/register">
+
+            </Route>
+
+            {/* Login page. Asks user for email and password. If succesfull, saves JWT to localstorage and redirects to "/". */}
+            <Route path="/Login">
             
-          </Route>
-
-          {/* Register page here. Asks user for relevant info. If registration succeeds, redirect to login. */}
-          <Route path="/register">
-
-          </Route>
-
-          {/* Login page. Asks user for email and password. If succesfull, saves JWT to localstorage and redirects to "/". */}
-          <Route path="/Login">
-          
-          </Route>
-        </Routes>
+            </Route>
+          </Routes>
       </BrowserRouter>
     </>
   )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const myTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#0b5fe6",
+    },
+    secondary: {
+      main: "#ffffff"
+    },
+  },
+})
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={myTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
