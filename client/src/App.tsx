ba58b724e89428ac65f3d86f196012fff8e679ad
Hannes Turppo <hannes.turppo@gmail.com>
2025-02-25 14:10:38 +0200

    Changed repo name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import MenuAppBar from "./components/appBar"
+import React, { useState } from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import MenuAppBar from "./components/menuAppBar"
 import { Box } from "@mui/material"
 import './App.css'
 
@@ -7,13 +8,26 @@ function App() {
 
   return (
     <>
-      <Box sx={{
-        border: 1,
-        width: "100vw",
-      }}>
+      <BrowserRouter>
         <MenuAppBar />
+        <Routes>
 
-      </Box>
+          {/* Homepage here shows user's collections. redirect to login if no token is found. */}
+          <Route path="/">
+            
+          </Route>
+
+          {/* Register page here. Asks user for relevant info. If registration succeeds, redirect to login. */}
+          <Route path="/register">
+
+          </Route>
+
+          {/* Login page. Asks user for email and password. If succesfull, saves JWT to localstorage and redirects to "/". */}
+          <Route path="/Login">
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
